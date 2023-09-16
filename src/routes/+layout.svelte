53c84c0400cb9f88175b040cc4855c8d55bad1a2
Samuel Aditia <samueladitia95@gmail.com>
2023-09-17 01:29:29 +0700

    feat: add no scrollbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import "../app.css";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		document.body.classList.add("no-scrollbar");
+		window.scrollTo(0, 0);
+	});
 </script>
 
 <slot />
