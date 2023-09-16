bf1db790e77742a396d6c5f1ece08c02424bbeea
Samuel Aditia <samuel.aditia@ruparupa.com>
2023-09-16 14:10:23 +0700

    feat: add color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ro-black": "#1D1D1B",
+        "ro-creme": "#B7B0A8",
+        "ro-light-creme": "#F7F4EF",
+        "ro-white": "#FFFFFF",
+        "ro-brown": "#4C4A44",
+        "ro-error": "#D10000"
+      }
+    },
   },
   plugins: [],
 }
