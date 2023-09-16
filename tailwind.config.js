dae6983f606975a74550fbefb56608e9a6a53955
Samuel Aditia <samuel.aditia@ruparupa.com>
2023-09-16 14:12:11 +0700

    feat: add vscode config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      colors: {
-        "ro-black": "#1D1D1B",
-        "ro-creme": "#B7B0A8",
-        "ro-light-creme": "#F7F4EF",
-        "ro-white": "#FFFFFF",
-        "ro-brown": "#4C4A44",
-        "ro-error": "#D10000"
-      }
-    },
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'ro-black': '#1D1D1B',
+				'ro-creme': '#B7B0A8',
+				'ro-light-creme': '#F7F4EF',
+				'ro-white': '#FFFFFF',
+				'ro-brown': '#4C4A44',
+				'ro-error': '#D10000'
+			}
+		}
+	},
+	plugins: []
+};
