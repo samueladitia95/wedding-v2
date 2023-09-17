d8227b518dc0a2f399b3774a287b5926becd75fa
Samuel Aditia <samueladitia95@gmail.com>
2023-09-17 22:51:27 +0700

    feat: change absolute value to flex

--- src/lib/containers/Navigation.svelte
+++ src/lib/containers/Navigation.svelte
@@ -1,14 +1,14 @@
 <div class="bg-ro-light-creme" id="nav-container">
-	<div class="min-h-screen min-w-screen relative">
+	<div
+		class="min-h-screen min-w-screen relative pt-16 pb-8 container flex flex-col justify-between items-stretch"
+	>
 		<!-- Main Logo -->
-		<div class="absolute left-1/2 transform -translate-x-1/2 top-16">
+		<div class="w-full flex justify-center">
 			<img src="/icons/logo-white.svg" alt="full name" class="w-12 h-12" />
 		</div>
 
 		<!-- Nav Item -->
-		<div
-			class="container absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-6 text-4xl font-juana font-light"
-		>
+		<div class="flex flex-col gap-6 text-4xl font-juana font-light">
 			<div class="text-left text-ro-nav">We Invite You</div>
 			<div class="text-right text-ro-nav">Our Story</div>
 			<div class="text-left text-ro-nav">Wedding Guide</div>
@@ -17,7 +17,7 @@
 		</div>
 
 		<!-- Full Name -->
-		<div class="absolute left-1/2 transform -translate-x-1/2 bottom-8">
+		<div class="w-full flex justify-center">
 			<img src="/icons/full-name.svg" alt="full name" class="w-28 h-[14px]" />
 		</div>
 	</div>
