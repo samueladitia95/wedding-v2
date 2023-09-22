e24668024cdfa8fe73c2f51b3850119cca023934
Samuel Aditia <samueladitia95@gmail.com>
2023-09-22 21:18:41 +0700

    refactor: group container folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Intro from "$lib/containers/Intro.svelte";
-	import Navigation from "$lib/containers/Navigation.svelte";
+	import Intro from "$lib/containers/Intro/Intro.svelte";
+	import Navigation from "$lib/containers/Intro/Navigation.svelte";
 </script>
 
 <Intro />
--- src/routes/invitation/+page.svelte
+++ src/routes/invitation/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import Footer from "$lib/components/Footer.svelte";
 	import Header from "$lib/components/Header.svelte";
-	import InviteCountdown from "$lib/containers/InviteCountdown.svelte";
-	import InviteIntro from "$lib/containers/InviteIntro.svelte";
+	import InviteCountdown from "$lib/containers/Invitation/InviteCountdown.svelte";
+	import InviteIntro from "$lib/containers/Invitation/InviteIntro.svelte";
 </script>
 
 <div>
