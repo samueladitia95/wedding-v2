cb62dd89fbb05134631bf1f10c0b1b7b748f4344
Samuel Aditia <samueladitia95@gmail.com>
2023-09-22 22:14:45 +0700

    feat: add animation to story videos

--- src/lib/containers/Story/StoryVideo.svelte
+++ src/lib/containers/Story/StoryVideo.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { inview } from "svelte-inview";
-	import { fade, fly } from "svelte/transition";
+	import { fade } from "svelte/transition";
 
 	let isShow = false;
 	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>): void => {
@@ -9,15 +9,15 @@
 </script>
 
 <div
-	class="relative min-h-screen min-w-full"
+	class="relative min-h-screen min-w-full bg-ro-brown"
 	use:inview={{
-		rootMargin: "0px",
+		rootMargin: "-100px",
 		unobserveOnEnter: true
 	}}
 	on:inview_change={handleChange}
 >
 	{#if isShow}
-		<div class="z-full absolute">
+		<div class="z-full absolute" in:fade={{ duration: 2000, delay: 1000 }}>
 			<!-- Need to change to youtube player to autoplay -->
 			<iframe
 				src="https://drive.google.com/file/d/1jC1k8t6ew4WIItndyfqmCyDDhv7SQBKu/preview?start=1"
