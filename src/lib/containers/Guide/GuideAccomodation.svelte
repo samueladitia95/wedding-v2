38758b8ea1e9d0adb064a4031f13842c07d77f6c
Samuel Aditia <samueladitia95@gmail.com>
2023-09-27 19:59:34 +0700

    feat: add responsive to guide accomodation

--- src/lib/containers/Guide/GuideAccomodation.svelte
+++ src/lib/containers/Guide/GuideAccomodation.svelte
@@ -5,7 +5,7 @@
 	let isShow = false;
 	const accomodations = [
 		{
-			logo: "/images/guide_3.png",
+			logo: "/images/guide_3.svg",
 			subject: "FAMILY",
 			location: "Hotel Episode Kuta Bali",
 			address: "Jl. Bypass Ngurai Rai No.99, Kuta, Kec. Kuta",
@@ -18,7 +18,7 @@
 			time: "2 Nights (Nov 16 - Nov 18, 2023)"
 		},
 		{
-			logo: "/images/guide_4.png",
+			logo: "/images/guide_4.svg",
 			subject: "FAMILY ONLY",
 			location: "Hotel Santika Nusa Dua - Church - Canna Bali - Hotel Episode Kita"
 		}
@@ -41,32 +41,41 @@
 	{#if isShow}
 		<div class="container">
 			<div class="min-h-screen bg-ro-light-creme text-ro-black text-center rounded-full p-5">
-				<div class="min-h-screen rounded-full border border-ro-brown py-28">
-					<div class="font-island text-3xl mb-10" in:fade={{ duration: 1000, delay: 500 }}>
+				<div class="min-h-screen rounded-full border border-ro-brown py-28 md:py-48 xl:py-72">
+					<div
+						class="font-island text-3xl md:text-5xl mb-10 md:mb-14"
+						in:fade={{ duration: 1000, delay: 500 }}
+					>
 						The Accomodations
 					</div>
 
-					<div class="flex flex-col gap-10 justify-start items-center px-2 font-mirage">
+					<div class="flex flex-col gap-10 md:gap-14 justify-start items-center px-2 font-mirage">
 						{#each accomodations as accomodation}
-							<div>
+							<div class="flex flex-col items-center">
 								{#if accomodation.logo}
-									<div class="flex items-center justify-center mb-8 md:mb-14">
-										<img src={accomodation.logo} alt="accomodation" class="h-[100px]" />
+									<div class="flex items-center justify-center mb-8">
+										<img
+											src={accomodation.logo}
+											alt="accomodation"
+											class="h-[100px] md:h-[200px]"
+										/>
 									</div>
 								{/if}
-								<div class="text-xl mb-6">{accomodation.subject}</div>
-								<div class="text-xl mb-1">{accomodation.location}</div>
+								<div class="text-xl md:text-4xl mb-6">{accomodation.subject}</div>
+								<div class="text-xl md:text-2xl md:leading-relaxed mb-1 md:mb-4 max-w-sm">
+									{accomodation.location}
+								</div>
 								{#if accomodation.address}
-									<div class="text-sm mb-1">{accomodation.address}</div>
+									<div class="text-sm md:text-xl mb-1 md:mb-4">{accomodation.address}</div>
 								{/if}
 								{#if accomodation.time}
-									<div class="text-lg mb-1">{accomodation.time}</div>
+									<div class="text-lg md:text-xl">{accomodation.time}</div>
 								{/if}
 							</div>
 						{/each}
 					</div>
 
-					<div class="text-sm font-mirage px-12">
+					<div class="text-sm md:text-xl font-mirage px-12">
 						Please be ready at the hotel lobby at 8.00 am (WITA)
 					</div>
 				</div>
