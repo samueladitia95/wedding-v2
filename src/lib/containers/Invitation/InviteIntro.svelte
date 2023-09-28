bb01258afd064b40afae28bcd92ab660b83a6689
Samuel Aditia <samuel.aditia@ruparupa.com>
2023-09-28 13:24:13 +0700

    feat: update transision in invite intro

--- src/lib/containers/Invitation/InviteIntro.svelte
+++ src/lib/containers/Invitation/InviteIntro.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { inview } from "svelte-inview";
-	import { fade } from "svelte/transition";
+	import { fade, fly } from "svelte/transition";
 
 	let isShow = false;
 	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>): void => {
@@ -16,17 +16,22 @@
 	}}
 	on:inview_change={handleChange}
 >
-	<!-- Main Image -->
-	<img
-		src="/images/invite_mobile_1.png"
-		alt="invite"
-		class="absolute top-0 left-0 w-screen h-screen object-cover object-left"
-	/>
-
-	<!-- Gradient Layer -->
-	<div class="h-screen w-screen absolute top-0 left-0 z-10 gradient-layer" />
 	{#if isShow}
-		<div class="absolute z-20 min-h-screen min-w-full" in:fade={{ duration: 2000, delay: 500 }}>
+		<!-- Main Image -->
+		<img
+			src="/images/invite_mobile_1.png"
+			alt="invite"
+			class="absolute top-0 left-0 w-screen h-screen object-cover object-left"
+		/>
+		<!-- Gradient Layer -->
+		<div
+			class="h-screen w-screen absolute top-0 left-0 z-10 gradient-layer"
+			in:fly={{ y: -1000, duration: 2000 }}
+		/>
+	{/if}
+
+	{#if isShow}
+		<div class="absolute z-20 min-h-screen min-w-full" in:fade={{ duration: 2000, delay: 1500 }}>
 			<!-- Main Section -->
 			<div class="container min-h-screen min-w-full flex flex-col items-center">
 				<div
