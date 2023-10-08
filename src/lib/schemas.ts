6e1b2758b35433c60222d08e47b62c48354265d5
rezafiansyah <rezafiansyah.putra@gmail.com>
2023-10-09 01:14:41 +0700

    change property name for rsvp (fix the most suitable feedback)

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -16,37 +16,37 @@ export const _schemaWishes = z
 
 export const _schemaRsvp = z
 	.object({
-		name: z
+		attendee_name: z
 			.string({ required_error: "Name is Required" })
 			.trim()
 			.min(1, { message: "Name is Required" }),
-		phone: z
+		attendee_phone: z
 			.string({
 				required_error: "Phone Number is Required"
 			})
 			.regex(/(^08)(\d{3,4}-?){2}\d{2,3}$/, "Phone Number is Invalid"),
-		email: z
+		attendee_email: z
 			.string({
 				required_error: "Email is Required"
 			})
 			.email({
 				message: "Email is Invalid"
 			}),
-		isAttending: z
+		is_attend: z
 			.boolean({
 				required_error: "is Attending is Required"
 			})
 			.default(false),
-		isDate: z
+		is_bring_partner: z
 			.boolean({
 				required_error: "Date is Required"
 			})
 			.default(false),
-		isAccomodation: z
+		is_need_accomodation: z
 			.boolean({
 				required_error: "Accomodation is Required"
 			})
 			.default(false),
-		allergies: z.string().trim()
+		attendee_note: z.string().trim()
 	})
 	.required();
